aa680028340e782ebe4111b190c587e2d94626a4
Aavni-Jon <aavnijon@gmail.com>
2025-03-19 08:06:30 -0700

    Add code for both eyes

--- RGB_Eyes.py
+++ RGB_Eyes.py
@@ -1,4 +1,3 @@
-
 from gpiozero import LED
 from gpiozero import PWMLED
 import time
@@ -8,25 +7,42 @@ debug_messages = 1 # If debug messages is 1 then message will be printed, else i
 if debug_messages : print("Debug Message are 'ON'")
 else : print("Debug Message are 'OFF'")
 
-# Raspberry Pi Pins
-red_pwm_pin = PWMLED(3) 
-green_pwm_pin = PWMLED(4) 
-blue_pwm_pin = PWMLED(17) 
+# Raspberry Pi Pins left eye
+redleft_pwm_pin = PWMLED(3)
+greenleft_pwm_pin = PWMLED(12)
+blueleft_pwm_pin = PWMLED(13)
+#Rasberry Pi Pind rigth eye
+redrigth_pwm_pin = PWMLED(16)
+greenrigth_pwm_pin = PWMLED(20)
+bluerigth_pwm_pin = PWMLED(21)
 
-def eyes_RGB(eyes_status):
-    if debug_messages : print("Running eyes_RGB function")
-    if debug_messages : print(eyes_status)
-    red,green,blue = eyes_status
-    red_pwm_pin.value = eyes_status[red]
-    green_pwm_pin.value = eyes_status[green]
-    blue_pwm_pin.value = eyes_status[blue]
+def eyes_left_RGB(eyes_left):
+    if debug_messages : print("Running lefteye_RGB function")
+    if debug_messages : print(eyes_left)
+    red,green,blue = eyes_left
+    redleft_pwm_pin.value = eyes_left[red]
+    greenleft_pwm_pin.value = eyes_left[green]
+    blueleft_pwm_pin.value = eyes_left[blue]
 
+def eyes_rigth_RGB(eyes_rigth):
+    if debug_messages : print("Running rigtheye_RGB function")
+    if debug_messages : print(eyes_rigth)
+    red,green,blue = eyes_rigth
+    redrigth_pwm_pin.value = eyes_rigth[red]
+    greenrigth_pwm_pin.value = eyes_rigth[green]
+    bluerigth_pwm_pin.value = eyes_rigth[blue]
 def main():
-    #print("Welcome To The STEAM Clown Makey Bot")
-    eyes_RGBLEDs = {'red_RGBLED':.1, 'green_RGBLED':.5, 'blue_RGBLED':.99}
+    print("Welcome To The STEAM Clown Makey Bot")
+    eyes_left_RGBLEDs = {'red_RGBLED':.4, 'green_RGBLED':.5, 'blue_RGBLED':0}
     #eyes_RGBLEDs = {'left_eye':{'leye_red_RGBLED':1, 'leye_green_RGBLED':.5, 'leye_blue_RGBLED':.99}, 'right_eye':{'reye_red_RGBLED':1, 'reye_green_RGBLED':.5, 'reye_blue_RGBLED':.99}}
-    if debug_messages : print("Calling eyes1_RGB function")
-    eyes_RGB(eyes_RGBLEDs)
+    if debug_messages : print("Calling eyes_RGB function")
+    eyes_left_RGB(eyes_left_RGBLEDs)
+   
+    eyes_rigth_RGBLEDs = {'red_RGBLED':.4, 'green_RGBLED':.5, 'blue_RGBLED':0}
+    #eyes_RGBLEDs = {'rigth_eye':{'leye_red_RGBLED':1, 'leye_green_RGBLED':.5, 'leye_blue_RGBLED':.99}, 'right_eye':{'reye_red_RGBLED':1, 'reye_green_RGBLED':.5, 'reye_blue_RGBLED':.99}}
+    if debug_messages : print("Calling eyes_RGB function")
+    eyes_rigth_RGB(eyes_rigth_RGBLEDs)
+   
     if debug_messages : print("Returned from eyes_RGB function")
 
 main()
